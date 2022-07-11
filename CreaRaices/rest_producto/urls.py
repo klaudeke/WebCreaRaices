7e112cd8371c8186e694bbac740608c52cf0daeb
klaudeke <102482268+klaudeke@users.noreply.github.com>
2022-07-11 14:30:37 -0400

    asd
    asd

--- CreaRaices/rest_producto/urls.py
+++ CreaRaices/rest_producto/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from rest_producto.viewsLogin import login
 from rest_producto.views import api_productos
 
 urlpatterns = [
     path('api_productos',api_productos,name="api_productos"),
+    path('login',login,name="login"),
 ]
---
+++ CreaRaices/rest_producto/viewsLogin.py
@@ -0,0 +1,36 @@
+from http.client import CREATED
+from lib2to3.pgen2 import token
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
+from rest_framework.authtoken.models import Token
+#from .models import Usuario
+
+
+@csrf_exempt
+@api_view(['POST'])
+def login(request):
+    
+    data = JSONParser().parse(request)
+    username = data['crearaices']
+    password = data['cr34R41c3s']
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response("Usuario incorrecto")
+    pass_valido = check_password(password, user.password)
+    if not pass_valido:
+        return Response("Password incorrecto")
+    
+    token, created = Token.objects.get_or_create(user=user)
+    return Response(token.key)
+
+
+
+
+# Create your views here.
